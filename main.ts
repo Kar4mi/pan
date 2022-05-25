478b284134ab68e67509c13c5925d162e976f7ac
Kar4mi <106243941+Kar4mi@users.noreply.github.com>
2022-05-25 13:47:12 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let a = 0
+let izq = 0
+let der = 0
 basic.forever(function () {
-	
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    a = maqueen.readPatrol(maqueen.Patrol.PatrolLeft) + maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    izq = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+    der = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    if (izq) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 225)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 26)
+        basic.pause(115)
+    }
+    if (der) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 225)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 26)
+        basic.pause(115)
+    }
+    if (a) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
